8fe79f099a357c34842857a034ce79931b9080e1
mixinan <mixinan@163.com>
2019-04-03 17:39:06 +0800

    重装系统前的备份

--- fenye/data/init.php
+++ fenye/data/init.php
@@ -1,6 +1,6 @@
 <?php
   //创建到服务器的连接
-    $conn=mysqli_connect("ip address","username","passwd","database",3306);
+    $conn=mysqli_connect("47.93.3.163","root","mixinan007!","zhishi",3306);
     $sql="SET NAMES UTF8";
 
 	mysqli_query($conn,$sql);
